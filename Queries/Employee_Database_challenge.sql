a69f96103b76027dd708f6d41ebe9fec3b0e6130
Henry Barze <henrybarze@Henrys-Air-2.lan>
2022-02-01 18:46:22 -0700

    final module 7 files

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,10 +1,11 @@
+--Deliverable Pt. 1:
 SELECT e.emp_no,
     e.first_name,
     e.last_name,
 	t.title,
 	t.from_date,
 	t.to_date
-INTO Deliverable1_Retirees_by_title
+INTO retirement_titles
 FROM employees as e
 LEFT JOIN titles as t
 ON (e.emp_no = t.emp_no)
@@ -17,7 +18,7 @@ rt.first_name,
 rt.last_name,
 rt.title
 INTO unique_titles
-FROM Deliverable1_Retirees_by_title AS rt
+FROM retirement_titles AS rt
 ORDER BY emp_no ASC, to_date DESC;
 
 SELECT 
@@ -47,4 +48,37 @@ WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY emp_no;
 
-SELECT * FROM mentor_eligibility;
+SELECT * FROM rolls_needed;
+
+--Deliverable 3
+SELECT DISTINCT ON (rt.emp_no) 
+	rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title,
+	d.dept_name
+INTO unique_titles_department
+FROM retirement_titles as rt
+INNER JOIN dept_emp as de
+ON (rt.emp_no = de.emp_no)
+INNER JOIN departments as d
+ON (d.dept_no = de.dept_no)
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Roles needed to be filled by title and dept. 
+SELECT ut.dept_name, ut.title, COUNT(ut.title) 
+INTO rolls_needed
+FROM (SELECT title, dept_name from unique_titles_department) as ut
+GROUP BY ut.dept_name, ut.title
+ORDER BY ut.dept_name DESC;
+
+-- Qualified staff, retirement-ready to mentor next generation.
+SELECT ut.dept_name, ut.title, COUNT(ut.title) 
+INTO qualified_staff
+FROM (SELECT title, dept_name from unique_titles_department) as ut
+WHERE ut.title IN ('Senior Engineer', 'Senior Staff', 'Technique Leader', 'Manager')
+GROUP BY ut.dept_name, ut.title
+ORDER BY ut.dept_name DESC;
+
+SELECT * FROM qualified_staff;
+
